497d0f3679894ef4400047f0d457cbd41e632b5f
Delgorithm <delgorithm@gmail.com>
2024-09-24 09:29:45 +0200

    Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: "/ragip-diler",
+				destination: "/ragip-diler/dashboard/home",
+				permanent: true,
+			},
+			{
+				source: "/ragip-diler/dashboard",
+				destination: "/ragip-diler/dashboard/home",
+				permanent: true,
+			},
+		];
+	},
+};
 
 export default nextConfig;
