eadab065dea0125b464ac0784d96d90511f9289c
Delgorithm <delgorithm@gmail.com>
2024-10-03 12:54:22 +0200

    Design : Anatolii Dmitrienko

---
+++ app/anatolii-dmitrienko/page.tsx
@@ -0,0 +1,44 @@
+import GridImages from '@/components/anatolii-dmitrienko/grid-images';
+
+{
+  /* Anatolii Dmitrienko */
+}
+
+export default function AnatoliiDmitrienkoPage() {
+  return (
+    <section className="watch-sm:m-8 h-screen watch-sm:text-sm">
+      <div className="grid watch-sm:grid-cols-1 watch-sm:grid-rows-17 watch-sm:gap-4">
+        <div className="watch-sm:row-span-3 border-[0.25px] border-neutral-50 shadow-sm rounded-xl">
+          <div>
+            <GridImages />
+          </div>
+          <p>
+            Enjoy a smooth, well-oragnized photoshoot, allwing you to focus on
+            creating memories while I handle the rest.
+          </p>
+        </div>
+        <div className="watch-sm:row-span-3 border-[0.25px] border-neutral-50 shadow-sm rounded-xl">
+          2
+        </div>
+        <div className="watch-sm:row-span-2 border-[0.25px] border-neutral-50 shadow-sm rounded-xl">
+          3
+        </div>
+        <div className="watch-sm:row-span-1 border-[0.25px] border-neutral-50 shadow-sm rounded-xl">
+          4
+        </div>
+        <div className="watch-sm:row-span-3 border-[0.25px] border-neutral-50 shadow-sm rounded-xl">
+          5
+        </div>
+        <div className="watch-sm:row-span-3 border-[0.25px] border-neutral-50 shadow-sm rounded-xl">
+          6
+        </div>
+        <div className="watch-sm:row-span-2 border-[0.25px] border-neutral-50 shadow-sm rounded-xl">
+          7
+        </div>
+        <div className="watch-sm:row-span-2 border-[0.25px] border-neutral-50 shadow-sm rounded-xl">
+          8
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/anatolii-dmitrienko/grid-images.tsx
@@ -0,0 +1,177 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+export default function GridImages() {
+  const gridFaces = [
+    {
+      img: '/anatolii-dmitrienko/grid-face-1.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-2.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-3.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-4.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-5.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-6.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-7.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-8.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-9.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-10.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-11.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-12.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-13.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-14.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-15.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-16.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-17.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-18.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-19.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-20.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-21.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-22.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-23.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-24.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-25.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-26.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-27.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-28.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-29.svg',
+    },
+    {
+      img: '/anatolii-dmitrienko/grid-face-30.svg',
+    },
+  ];
+
+  const firstGridFaces = gridFaces.slice(0, 10);
+  const secondGridFaces = gridFaces.slice(10, 20);
+  const thirdGridFaces = gridFaces.slice(20, 30);
+
+  return (
+    <section className="overflow-hidden">
+      <motion.div
+        className="flex items-center gap-[0.5px] overflow-hidden "
+        animate={{
+          x: [-350, 0, -350],
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 45,
+          ease: 'linear',
+        }}
+        style={{ width: 'max-content' }}
+      >
+        {firstGridFaces.map((item, img) => (
+          <Image
+            key={img}
+            src={item.img}
+            width={400}
+            height={400}
+            alt="People faces"
+            className="size-16"
+          />
+        ))}
+      </motion.div>
+      <motion.div
+        className="flex items-center gap-[0.5px] overflow-hidden "
+        animate={{
+          x: [0, -350, 0],
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 45,
+          ease: 'linear',
+        }}
+        style={{ width: 'max-content' }}
+      >
+        {secondGridFaces.map((item, img) => (
+          <Image
+            key={img}
+            src={item.img}
+            width={400}
+            height={400}
+            alt="People faces"
+            className="size-16"
+          />
+        ))}
+      </motion.div>
+      <motion.div
+        className="flex items-center gap-[0.5px] overflow-hidden "
+        animate={{
+          x: [-330, 0, -330],
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 45,
+          ease: 'linear',
+        }}
+        style={{ width: 'max-content' }}
+      >
+        {thirdGridFaces.map((item, img) => (
+          <Image
+            key={img}
+            src={item.img}
+            width={400}
+            height={400}
+            alt="People faces"
+            className="size-16"
+          />
+        ))}
+      </motion.div>
+    </section>
+  );
+}
